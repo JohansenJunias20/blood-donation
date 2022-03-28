1e52fad2d6623f3a42d11d4e6f9dba7bbf26961e
Quetzalcoalts <99593775+Quetzalcoalts@users.noreply.github.com>
2022-03-28 11:29:53 +0700

    Update index.php
    aaaaa

--- web/index.php
+++ web/index.php
@@ -10,7 +10,7 @@ include_once("autoload.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-    <h1>Hello World</h1>
+    <h1>Hello aaaaa</h1>
     <?php 
         echo request("http://python/predict?p1=0&p2=30");
     ?>

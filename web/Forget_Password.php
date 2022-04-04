2ab1ec727d3db0f73899fe37a638452e9dd03eed
danielmp06 <c14190086@john.petra.ac.id>
2022-04-04 14:44:08 +0700

    ganti" ui

--- web/Forget_Password.php
+++ web/Forget_Password.php
@@ -8,45 +8,82 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+    <style>
+        body {
+            background: linear-gradient(70deg, #e6e6e6, #f7f7f7);
+            color: #514B64;
+        }
+        hr{
+            border-width: 1px;
+            border-style: solid none;
+            border-color: #BB002D;
+        }
+        .login-form {
+            width: 340px;
+            margin: 50px auto;
+        }
+        .login-form form {
+            margin-bottom: 15px;
+            background: #f7f7f7;
+            box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+            padding: 30px;
+        }
+        .login-form h2 {
+            margin: 0 0 15px;
+        }
+        .form-control, .btn {
+            min-height: 38px;
+            border-radius: 2px;
+        }
+        .btn {        
+            font-size: 15px;
+            font-weight: bold;
+            background-color: #BB002D;
+            color: #f7f7f7;
+            border: none;
+        }
+        .btn:hover{
+            background-color:#F6B3B3;
+        }
+    </style>
 </head>
 
-
 <body>
-    <div class="container">
-        <div class="card" style="width: 50%; margin-left: auto; margin-right: auto;">
-            <div class="card-body">
-                <h2 class="card-title" style="font-size: 4vw; text-align: center; margin-bottom: 2vw;">Forget Password</h2>
-                <form>
-                    <div class="form-group" style="margin-bottom: 2vw;">
-                        <label for="InputEmail1" style="font-size: 2vw;">Email address</label>
-                        <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email" style="font-size: 2vw;">
-                    </div>
-                    
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#Forget_Password_Modal" style="font-size: 2vw; width: 100%;">Submit</button>
-                    <!-- Modal -->
-                    <div class="modal fade" id="Forget_Password_Modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">New Password has been send to your email</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                <p>Please check your email to login with new random password and change your password at profile. Thankyou</p>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                </div>
-                            </div>
+    <div class="login-form">
+        <form action="/examples/actions/confirmation.php" method="post">
+            <div style="width:100%; text-align:center">
+                <img src="asset/Icon.png" width="200" alt="" >
+                
+            </div>
+            <hr>
+            <h2 class="text-center" style="color: black;">Reset Password</h2>       
+            <div class="form-group">
+                <input type="text" class="form-control" id="RegisterEmail" placeholder="Email" style="font-size: 2sp;">
+            </div>
+            <br>
+            <div class="form-group">
+                <button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#Forget_Password_Modal">Submit</button>
+            </div>
+            <div class="modal fade" id="Forget_Password_Modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">New Password has been send to your email</h5>
+                        </div>
+                        <div class="modal-body">
+                            <p>Please check your email to login with new random password and change your password at profile. Thank you!</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                         </div>
                     </div>
-                </form>
+                </div>
             </div>
-        </div>
+            <div class="botomline">
+                <p class="text-center"><a href="Login.php">Already have an account?</a></p>
+            </div>        
+        </form>
     </div>
-    
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
--- web/Login.php
+++ web/Login.php
@@ -8,29 +8,69 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+    <style>
+        body {
+            background: linear-gradient(70deg, #e6e6e6, #f7f7f7);
+            color: #514B64;
+        }
+        hr{
+            border-width: 1px;
+            border-style: solid none;
+            border-color: #BB002D;
+        }
+        .login-form {
+            width: 340px;
+            margin: 50px auto;
+        }
+        .login-form form {
+            margin-bottom: 15px;
+            background: #f7f7f7;
+            box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+            padding: 30px;
+        }
+        .login-form h2 {
+            margin: 0 0 15px;
+        }
+        .form-control, .btn {
+            min-height: 38px;
+            border-radius: 2px;
+        }
+        .btn {        
+            font-size: 15px;
+            font-weight: bold;
+            background-color: #BB002D;
+            color: #f7f7f7;
+            border: none;
+        }
+        .btn:hover{
+            background-color:#F6B3B3;
+        }
+    </style>
 </head>
 
-
 <body>
-    <div class="container">
-        <div class="card" style="width: 50%; margin-left: auto; margin-right: auto;">
-            <div class="card-body">
-                <h2 class="card-title" style="font-size: 4vw; text-align: center; margin-bottom: 5vw;">Login</h2>
-                <form>
-                    <div class="form-group">
-                        <label for="InputUsername" style="font-size: 2vw;">Username</label>
-                        <input type="text" class="form-control" id="InputUsername" placeholder="Enter username" style="font-size: 2vw;">
-                    </div>
-                    <div class="form-group" style="margin-bottom: 3vw;">
-                        <label for="exampleInputPassword1" style="font-size: 2vw;">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style="font-size: 2vw;">
-                    </div>
-                    <a href="profile.php" type="submit" class="btn btn-primary" style="font-size: 2vw; width: 100%;">Login</a>
-                    <small id="forgetpassword" class="form-text"><a href="Forget_Password.php" style="font-size: 1vw;">Forget Password</a></small>
-                    <a href="Register.php" type="button" class="btn btn-primary" style="font-size: 2vw; margin-top: 1vw; width: 100%;">Register</a>
-                </form>
+    <div class="login-form">
+        <form action="/examples/actions/confirmation.php" method="post">
+            <div style="width:100%; text-align:center">
+                <img src="asset/Icon.png" width="200" alt="" >
+                
             </div>
-        </div>
+            <hr>
+            <h2 class="text-center" style="color: black;">Login</h2>       
+            <div class="form-group">
+                <input type="text" class="form-control" id="InputUsername" placeholder="Username" style="font-size: 2sp;">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" id="InputPassword"placeholder="Password" style="font-size: 2sp;">
+            </div>
+            <div class="form-group">
+                <a type="submit" href="profile.php" class="btn btn-primary btn-block">Login</a>
+            </div>
+            <div class="botomline">
+                <a href="Register.php" class="float-left">Create an Account!</a>
+                <a href="Forget_Password.php" class="float-right" >Forgot Password?</a>
+            </div>        
+        </form>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
